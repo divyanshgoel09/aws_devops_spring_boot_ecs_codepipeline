770720e5f5a605b6a2c652fdc86532146fb2920a
Vimal Daga <vdaga@lwindia.com>
2024-09-24 21:05:33 +0530

    new changes in code

--- src/main/java/com/linuxworld/controller/CourseController.java
+++ src/main/java/com/linuxworld/controller/CourseController.java
@@ -65,7 +65,7 @@ public class CourseController {
 
     @GetMapping("/welcome")
     public String greetings() {
-        return "Hello LW , AWS CICD Example working fine !";
+        return "Hello LW , AWS CICD welcomes you all at LW !";
     }
 
 
