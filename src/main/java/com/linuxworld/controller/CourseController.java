88f60a38e468630000891521e8ae45738383f137
Vimal Daga <vdaga@lwindia.com>
2024-09-24 22:01:54 +0530

    final

--- src/main/java/com/linuxworld/controller/CourseController.java
+++ src/main/java/com/linuxworld/controller/CourseController.java
@@ -65,7 +65,7 @@ public class CourseController {
 
     @GetMapping("/welcome")
     public String greetings() {
-        return "Hello LW , AWS CICD welcomes you all at LW !";
+        return "Hello LW , Final Tc";
     }
 
 
