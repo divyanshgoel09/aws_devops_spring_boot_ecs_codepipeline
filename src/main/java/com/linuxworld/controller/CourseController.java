d545c58b6851e16125d8cca607aafab1b5882b7d
Vimal Daga <vdaga@lwindia.com>
2024-09-30 20:15:15 +0530

    final

--- src/main/java/com/linuxworld/controller/CourseController.java
+++ src/main/java/com/linuxworld/controller/CourseController.java
@@ -65,7 +65,7 @@ public class CourseController {
 
     @GetMapping("/welcome")
     public String greetings() {
-        return "Hello LW , Workshop of Devops 3rd changes !! ";
+        return "Hello LW , Workshop of Devops FINAL changes !! ";
     }
 
 
