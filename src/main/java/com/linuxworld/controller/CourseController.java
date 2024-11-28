a7eaf6f7e7921757eb822995787a8297aca7f086
divyanshgoel09 <divyanshgoel70@gmail.com>
2024-11-28 16:29:06 +0530

    again some changes at 4:28 pm

--- src/main/java/com/linuxworld/controller/CourseController.java
+++ src/main/java/com/linuxworld/controller/CourseController.java
@@ -65,7 +65,7 @@ public class CourseController {
 
     @GetMapping("/welcome")
     public String greetings() {
-        return "Hello LW , Workshop of Devops!! ";
+        return "Hello LW , Workshop of Devops!! again changing 4:28 pm ";
     }
 
 
